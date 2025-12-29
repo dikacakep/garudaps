ffd88778a822f03999e0f15e09f0dd992ec0bb66
ryhndastra <ryhndastra@gmail.com>
2025-12-29 12:42:22 +0700

    fix typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,21 @@
-import Hero from "@/components/sections/Hero"
-import TutorialSection from "@/components/sections/Tutorial/TutorialSection"
-import Features from "@/components/sections/Features."
-import About from "@/components/sections/About"
-import Community from "@/components/sections/Community."
-import Teams from "@/components/sections/Teams"
-// import Footer from "@/components/layout/Footer"
-import FloatingButtons from "@/components/effects/FloatingButtons"
-import Navbar from "@/components/layout/Navbar"
+import Hero from "@/components/sections/Hero";
+import TutorialSection from "@/components/sections/Tutorial/TutorialSection";
+import Features from "@/components/sections/Features"; 
+import About from "@/components/sections/About";
+import Community from "@/components/sections/Community"; 
+import Teams from "@/components/sections/Teams";
+import FloatingButtons from "@/components/effects/FloatingButtons";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-background text-foreground selection:bg-primary selection:text-primary-foreground">
-      <Navbar />
+    <main>
       <Hero />
       <TutorialSection />
       <Features />
       <About />
       <Community />
       <Teams />
-      {/* <Footer /> */}
       <FloatingButtons />
     </main>
-  )
+  );
 }
