d104aace44c5f18dcba78eeb0296fc28375df317
ryhndastra <ryhndastra@gmail.com>
2026-01-27 00:38:51 +0700

    fix api address

--- src/app/api/proxy-status/route.ts
+++ src/app/api/proxy-status/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
-    const response = await fetch("https://api.garudacache.site/api/status", {
+    const response = await fetch("https://api.gtpscache.site/api/status", {
       cache: 'no-store', 
     });
     
